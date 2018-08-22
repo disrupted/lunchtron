72eb82d2b4bd05d28521cf42752c5cc96416dd90
disrupted <salomon.popp@gmail.com>
2018-08-22 17:48:41 +0200

    reenable basic auth

--- lunchtron/__init__.py
+++ lunchtron/__init__.py
@@ -1,8 +1,20 @@
+"""Lunchtron REST API."""
+import logging
+
 from flask import Flask, Blueprint
 from flask_restplus import Api, Resource, fields
 
-from lunchtron.models import User
+from passlib.hash import sha256_crypt
+from flask_httpauth import HTTPBasicAuth
+from lunchtron.models import Card, User, Admin, Checkin
 from lunchtron.database import db_session
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+_LOGGER = logging.getLogger(__name__)
 
 application = Flask(__name__)
 
@@ -16,15 +28,40 @@ application.url_map.strict_slashes = False
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint, doc='/documentation')  # doc=False
 application.register_blueprint(blueprint)
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(username, password_candidate):
+    """Verify basic auth credentials."""
+    admin = db_session.query(Admin).filter_by(username=username).first()
+    if not admin:
+        return False
+    return sha256_crypt.verify(password_candidate, admin.password_hash)
+
 
 api.namespaces.clear()  # get rid of default namespace
 api_users = api.namespace('users', description='USER operations')
+api_checkins = api.namespace('checkins', description='CHECKIN operations')
+api_cards = api.namespace('cards', description='CARD operations')
+
 
 users = api.model('Model', {
-    'user_uid': fields.Integer,
+    'id': fields.Integer,
     'name': fields.String,
     'price': fields.Fixed(description='fixed-precision decimal', decimals=2),
     'balance': fields.Fixed(description='fixed-precision decimal', decimals=2)
+})
+
+checkins = api.model('Model', {
+    'checkin_uid': fields.Integer,
+    'user_uid': fields.Integer,
+    'when': fields.DateTime(dt_format='rfc822')
+})
+
+cards = api.model('Model', {
+    'card_uid': fields.String,
+    'user_uid': fields.Integer
 })
 
 
@@ -38,15 +75,11 @@ def index():
     return 'ProVeg International'
 
 
-# @application.route('/api/users')
-# def users():
-#     users = db_session.query(User).all()
-#     return json.jsonify([user.to_dict() for user in users])
-
 @api_users.route('/')
 class UserList(Resource):
     """Shows a list of all users and lets you POST to add new users"""
 
+    @auth.login_required
     @api_users.marshal_with(users)
     def get(self):
         """List all users"""
@@ -57,9 +90,30 @@ class UserList(Resource):
 class UserDetail(Resource):
     """Show a single user."""
 
-    # @auth.login_required
+    @auth.login_required
     @api_users.doc('get_user')
     @api_users.marshal_with(users)
     def get(self, id):
         """List user by id"""
-        return db_session.query(User).filter(User.user_uid == id).one(), 200
+        return db_session.query(User).filter_by(id=id).one(), 200
+
+
+@api_checkins.route('/')
+class CheckinList(Resource):
+    """Checkins."""
+
+    # @auth.login_required
+    @api_users.doc('query all checkin data')
+    @api_checkins.marshal_with(checkins)
+    def get(self):
+        """List all checkins"""
+        return db_session.query(Checkin).all(), 200
+
+    # @api_checkins.doc('create_checkin')
+    # @api_checkins.expect(checkins)
+    # # @api_checkins.marshal_with(checkins, code=201)
+    # def post(self):
+    #     """Create new checkin"""
+    #     checkin_uid = db.create_checkin(api.payload['user_uid'])
+    #     return {'checkin_uid': checkin_uid, 'result': 'Checkin added'}, 201
+    #     # TODO return new checkin object, missing payload / argument
